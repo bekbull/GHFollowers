f84bf0b007fd7ceadda625d365f0b9191dbfe046
Bekbol Bolatov <bekbolbolatov@gmail.com>
2023-05-05 11:41:44 +0600

    init commit

---
+++ GHFollowers/FavoritesListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesListViewController.swift
+//  GHFollowers
+//
+//  Created by Bekbol Bolatov on 04.05.2023.
+//
+
+import UIKit
+
+class FavoritesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
---
+++ GHFollowers/SearchViewController.swift
@@ -0,0 +1,81 @@
+//
+//  SearchViewController.swift
+//  GHFollowers
+//
+//  Created by Bekbol Bolatov on 04.05.2023.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    let logoImageView = UIImageView()
+    let usernameTextField = GFTextField()
+    let callToFunctionButton = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+        configureLogoImageView()
+        configureTextField()
+        configureCallToActionButton()
+        createDismissKeyboardTapGesture()
+    }
+    
+    func createDismissKeyboardTapGesture() {
+        let tap = UITapGestureRecognizer(
+            target: self.view,
+            action: #selector(UIView.endEditing(_:))
+        )
+        view.addGestureRecognizer(tap)
+    }
+    
+    func configureLogoImageView() {
+        view.addSubview(logoImageView)
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.image = UIImage(named: "gh-logo")!
+        
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.heightAnchor.constraint(equalToConstant: 200),
+            logoImageView.widthAnchor.constraint(equalToConstant: 200)
+        ])
+    }
+    
+    func configureTextField() {
+        view.addSubview(usernameTextField)
+        
+        usernameTextField.delegate = self
+        
+        NSLayoutConstraint.activate([
+            usernameTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 48),
+            usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            usernameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor ,constant: -50),
+            usernameTextField.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    func configureCallToActionButton() {
+        view.addSubview(callToFunctionButton)
+        
+        NSLayoutConstraint.activate([
+            callToFunctionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            callToFunctionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            callToFunctionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            callToFunctionButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+}
+
+extension SearchViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("return key is tapped")
+        return true
+    }
+}
